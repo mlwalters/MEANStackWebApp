2a09cb9bb2c80a8c62c563dae4033ee38efca712
Maricar Walters <walterskai7@gmail.com>
2021-05-26 15:38:31 -0500

    Added new components post-list and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule} from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 
 @NgModule({
-  declarations: [
+  declarations: [ // For components
     AppComponent, 
-    PostCreateComponent
+    PostCreateComponent, 
+    HeaderComponent, 
+    PostListComponent
   ],
-  imports: [
+  imports: [  // For packages
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts = [
+    { title: "First Post", content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quos asperiores architecto? Explicabo quod dicta consectetur ipsa, sed quaerat eum recusandae laborum quae?"},
+    { title: "Second Post", content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quos asperiores architecto? Explicabo quod dicta consectetur ipsa, sed quaerat eum recusandae laborum quae?"},
+    { title: "Third Post", content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quos asperiores architecto? Explicabo quod dicta consectetur ipsa, sed quaerat eum recusandae laborum quae?"},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
